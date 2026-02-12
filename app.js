0078d1995b256f1a2d00373ba1e77acbd5ad054c
Tharaka Saranga <tharakasaranga755@gmail.com>
2026-02-13 01:20:21 +0530

    updated

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-const tokenRoutes = require('./routes/tokenRoutes');
-const { errorHandler } = require('./middleware/errorMiddleware');
+const express = require("express");
+const cors = require("cors");
+const tokenRoutes = require("./routes/tokenRoutes");
+const { errorHandler } = require("./middleware/errorMiddleware");
 const app = express();
 
+app.use(
+  cors({
+    origin: [
+      "https://token-management-eight.vercel.app",
+      "http://localhost:3000",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 
-//app. 
-app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Routes
-app.use('/api/tokens', tokenRoutes);
+app.use("/api/tokens", tokenRoutes);
 
-// Middleware error hanlding
 app.use(errorHandler);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const app = require('./app');
-
+const dotenv = require("dotenv");
 dotenv.config();
+
+const connectDB = require("./config/db");
+const app = require("./app");
+
 connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 }
 
 module.exports = app;
