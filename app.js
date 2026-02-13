66a8a155a39ac5428977bb2e0c6075013cb9f777
Tharaka Saranga <tharakasaranga755@gmail.com>
2026-02-13 09:35:46 +0530

    updated

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const tokenRoutes = require("./routes/tokenRoutes");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const app = express();
 
+// CORS Settings
 app.use(
   cors({
     origin: "*",
@@ -18,7 +19,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/tokens", tokenRoutes);
-
 app.use(errorHandler);
 
 module.exports = app;
