e1f6ddb6c650cc7c8cfddd6daf6bae81dd09d363
Tharaka Saranga <tharakasaranga755@gmail.com>
2026-02-13 09:33:00 +0530

    updated

--- app.js
+++ app.js
@@ -8,15 +8,17 @@ app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
+    optionsSuccessStatus: 200,
   }),
 );
 
-app.options("*", cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/tokens", tokenRoutes);
+
 app.use(errorHandler);
 
 module.exports = app;
