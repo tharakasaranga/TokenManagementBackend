e547a620e087627f32ab8b3c0675ebf65f80ceb2
Tharaka Saranga <tharakasaranga755@gmail.com>
2026-02-13 10:54:38 +0530

    updated

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 const app = express();
 
 const corsOptions = {
-  origin: "*",
+  origin: [
+    "https://token-management-4n87002m5-tharakasarangas-projects.vercel.app",
+    "http://localhost:3000",
+    "*"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
@@ -16,6 +20,17 @@ app.options("*", cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("API is Running Successfully!");
